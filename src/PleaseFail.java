afdb6812abc419ed910f68350bbf09a743f885d5
Pippin Bacon <151688865+Pippin404@users.noreply.github.com>
2024-09-05 12:32:23 -0400

    Wow I am bad at java. this should work
    trying to fix my own simple code

--- src/PleaseFail.java
+++ src/PleaseFail.java
@@ -1,6 +1,6 @@
-class{
-  public static void main(String[] args){
-      System.out.println("Hello");
-  }
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
-  
