0a90a80e5198e9283407e48c69d3f483ae054275
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-15 17:51:36 -0400

    Update and rename dataPackage.java to DataPackage.java

---
+++ src/apis/DataPackage.java
@@ -0,0 +1,28 @@
+interface DataPackage{
+	private void data();
+	private void message();	
+	
+	public void setData(int i) {
+			data=i;
+	}
+		
+	public int getData() {
+		if(data!=null) {
+		return data;
+		}		
+	return 0;
+	}
+		
+	public void setMessage(string s) {
+		message=s;
+	}
+		
+	public string getMessage() {
+		if(message==null) {
+			var n="Default Message";
+			return n;
+		}else{
+			return message;
+		}
+	}
+}
