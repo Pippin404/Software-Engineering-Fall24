b9ffeb073a72548ca83aa7713e1d19a375c0a277
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-15 17:49:37 -0400

    Update and rename dataBaseHandler.java to DataBaseHandler.java

---
+++ src/apis/DataBaseHandler.java
@@ -0,0 +1,20 @@
+public class DataBaseHandler implements dataPackage{
+		
+	public int writingNumber();
+		
+	public void storedData(dataPackage d){
+		//Prints out the d.getMessage
+		//stores it to a print writer file? probably
+	}
+	
+	public dataPackage sendData(){
+		//gets the data from the file (if there is any)
+		//parses it to an int
+		//adds it to a new datapackage
+		//adds a message to the datapackage like "returning data"
+		//sends it back
+	
+		
+	}
+		
+}
