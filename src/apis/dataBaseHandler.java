a9bc7f7f10ea508d753632196b3f950937d8e5d3
Pippin404 <Baconpippin@gmail.com>
2024-09-15 17:08:25 -0400

    Added data storage interface and protype class.
    May need adjusting later

---
+++ src/apis/dataBaseHandler.java
@@ -0,0 +1,25 @@
+public class dataBaseHandler implements dataPackage{
+	
+	public int writingNumber;
+	
+	public void storedData(dataPackage D){
+		//Prints out the d.getMessage
+		//stores it to a print writer file? probably
+		
+	}
+	
+	public dataPackage sendData(){
+		//gets the data from the file (if there is any)
+		//parses it to an int
+		//adds it to a new datapackage
+		//adds a message to the datapackage like "returning data"
+		//sends it back
+	
+	
+	}
+	
+		
+}
+//	 inputs a data package
+// - stores a data package to file
+// - sends a data package
---
+++ src/apis/dataPackage.java
@@ -0,0 +1,29 @@
+interface dataPackage{
+	private void data;
+	private void message;
+	
+	
+	public void setData(int i) {
+		data=i;
+	}
+	
+	public int getData() {
+		if(data!=null) {
+			return data;
+		}
+		return 0;
+	}
+	
+	public void setMessage(string s) {
+		message=s;
+	}
+	
+	public string getMessage() {
+		if(message==null) {
+			var n="Default Message"
+			return n;
+		}else{
+			return message
+		}
+	}
+}
