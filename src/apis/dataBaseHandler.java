efdcb32910180799711e88f153dc70773bb943a6
Pippin404 <Baconpippin@gmail.com>
2024-09-15 17:17:58 -0400

    small checkstyle errors

--- src/apis/dataBaseHandler.java
+++ src/apis/dataBaseHandler.java
@@ -1,25 +1,22 @@
-public class dataBaseHandler implements dataPackage{
-	
-	public int writingNumber();
-	
-	public void storedData(dataPackage D){
-		//Prints out the d.getMessage
-		//stores it to a print writer file? probably
+public class dataBaseHandler implements dataPackage(){
 		
-	}
-	
-	public dataPackage sendData(){
-		//gets the data from the file (if there is any)
-		//parses it to an int
-		//adds it to a new datapackage
-		//adds a message to the datapackage like "returning data"
-		//sends it back
-	
-	
-	}
-	
+		public int writingNumber();
 		
-}
-//	 inputs a data package
-// - stores a data package to file
-// - sends a data package
+		public void storedData(dataPackage D){
+			//Prints out the d.getMessage
+			//stores it to a print writer file? probably
+			
+		}
+		
+		public dataPackage sendData(){
+			//gets the data from the file (if there is any)
+			//parses it to an int
+			//adds it to a new datapackage
+			//adds a message to the datapackage like "returning data"
+			//sends it back
+		
+		
+		}
+		
+			
+	}
--- src/apis/dataPackage.java
+++ src/apis/dataPackage.java
@@ -1,29 +1,29 @@
 interface dataPackage{
-	private void data();
-	private void message();
+		private void data();
+		private void message();
 	
 	
-	public void setData(int i) {
-		data=i;
-	}
-	
-	public int getData() {
-		if(data!=null) {
-			return data;
+		public void setData(int i) {
+			data=i;
 		}
-		return 0;
-	}
-	
-	public void setMessage(string s) {
-		message=s;
-	}
-	
-	public string getMessage() {
-		if(message==null) {
-			var n="Default Message";
-			return n;
-		}else{
-			return message;
+		
+		public int getData() {
+			if(data!=null) {
+				return data;
+			}
+			return 0;
+		}
+		
+		public void setMessage(string s) {
+			message=s;
+		}
+		
+		public string getMessage() {
+			if(message==null) {
+				var n="Default Message";
+				return n;
+			}else{
+				return message;
+			}
 		}
 	}
-}
