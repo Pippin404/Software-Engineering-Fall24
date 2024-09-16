538e6b4cd35fdc5c0968027c1debe9516cde3d8c
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-16 11:19:04 -0400

    Update and rename Folders need items inside them to upload.TXT to InternalComputeEngine.java

---
+++ src/InternalComputeEngine.java
@@ -0,0 +1,115 @@
+public class InternalComputeEngine implements ComputeEngineInterface {
+
+    // Placeholder Values
+    private Integer data = null;
+    private String message = null;
+
+    @Override
+    public void setData(int i) {
+       data = i;
+    }
+
+    @Override
+    public int getData() {
+		    if (data != null) {
+            return data;
+        }
+    return 0;
+    }
+
+    @Override
+    public int compute(int i) {
+        return 1;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Horse:
+
+             _|\ _/|_,
+           ,((\\``-\\\\_
+         ,(())      `))\
+       ,(()))       ,_ \
+      ((())'   |        \
+      )))))     >.__     \
+      ((('     /    `-. .c|
+              /        `-`'
