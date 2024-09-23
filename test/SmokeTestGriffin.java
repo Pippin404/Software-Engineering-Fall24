004b34bf781fc99c320466b28980d1d8594a7769
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-23 16:44:22 -0400

    Update SmokeTestGriffin.java

--- test/SmokeTestGriffin.java
+++ test/SmokeTestGriffin.java
@@ -1,5 +1,30 @@
 package apis;
 
 public class SmokeTestGriffin {
+    public static void main(String[] args) {
+        InternalComputeEngine engine = new InternalComputeEngine();
 
+        // testing set and get data
+        engine.setData(5);
+        if (engine.getData() == 5) {
+            System.out.println("setData and getData passed.");
+        } else {
+            System.out.println("setData and getData failed.");
+        }
+
+        // testing compute() method
+        if (engine.compute(10) == 1) {
+            System.out.println("compute method passed (placeholder logic).");
+        } else {
+            System.out.println("compute method failed.");
+        }
+
+        // Smoke test for readInData
+        if (engine.readInData("test") == 0) {
+            System.out.println("readInData method passed.");
+        } else {
+            System.out.println("readInData method failed.");
+        }
+
+        
 }
