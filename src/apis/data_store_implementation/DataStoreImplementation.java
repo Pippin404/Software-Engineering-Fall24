af3722f41dc9d7c00deb1c713cb8c70cedef7a17
eribrin <joneserin650@gmail.com>
2024-09-22 22:40:55 -0400

    feat: empty implementation of DataStore, not sure if this is the right way to do it

---
+++ src/apis/data_store_implementation/DataStoreImplementation.java
@@ -0,0 +1,53 @@
+package apis.data_store_implementation;
+
+import apis.data_store.*;
+
+import java.util.Iterator;
+
+public class DataStoreImplementation implements DataStore {
+    private DataStore dataStore;
+    private Iterable<Integer> source;
+    private DestinationConfig destination;
+
+    public Iterable<Integer> read(SourceConfig source) {
+        return new Iterable<Integer>() {
+            @Override
+            public Iterator<Integer> iterator() {
+                return null;
+            }
+        };
+    }
+
+    public WriteResult appendSingleResult(DestinationConfig output, String result){
+        return new WriteResult(){
+
+        };
+    }
+
+    public SendSourceResponse sendSource(SendSourceRequest sendSourceRequest){
+        return new SendSourceResponse() {
+        };
+    }
+
+    public SendDestinationResponse sendDestination(SendDestinationRequest sendDestinationRequest){
+        return new SendDestinationResponse() {
+        };
+    }
+
+    public DelimiterInformation getDelimiterType(){
+        return DelimiterInformation.CHOSEN_DELIMITERS;
+    }
+
+    public ChooseDefaultDelimitersResponse chooseDelimiters(ChooseDefaultDelimitersRequest chooseDefaultDelimitersRequest){
+        return new ChooseDefaultDelimitersResponse() {
+        };
+    }
+
+    public SendDelimitersResponse sendDelimiters(SendDelimitersRequest sendDelimitersRequest){
+        return new SendDelimitersResponse() {
+        };
+    }
+
+
+
+}
