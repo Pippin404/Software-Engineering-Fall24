9e7f35727630bb7407d73c80fa81b1769245dc6a
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-23 00:54:58 -0400

    Create TestInputConfig.java

---
+++ test/test_utils/TestInputConfig.java
@@ -0,0 +1,15 @@
+package test_utils;
+
+import java.util.List;
+
+public class TestInputConfig {
+    private List<Integer> inputData;
+
+    public TestInputConfig(List<Integer> inputData) {
+        this.inputData = inputData;
+    }
+
+    public List<Integer> getInputData() {
+        return inputData;
+    }
+}
