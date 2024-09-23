5c36405ca31a3191427b295d2caa42d49acb9758
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-23 15:04:24 -0400

    Delete src/griffinapi/InternalComputeReadingInterface.java

--- src/griffinapi/InternalComputeReadingInterface.java
+++
@@ -1,9 +0,0 @@
-package griffinapi;
-
-interface InternalComputeReadingInterface {
-    // Reads in input and turns it into an int
-    void setData(int i);
-    int getData();
-    int readInData(String i);
-  
-}
