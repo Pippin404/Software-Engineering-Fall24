2b227bb7443fd7f8a32b8cb9de7cd527acdf9668
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-26 11:13:28 -0400

    Update IntegrationTest.java

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,47 +1,37 @@
-package test;
-
-import griffinapi.InternalComputeReadingInterface;
-import griffinapi.InternalComputeReading;
-import griffinapi.InternalComputeEngineInterface;
-import griffinapi.InternalComputeEngine;
-import test_utils.TestInputConfig;
-import test_utils.TestOutputConfig;
-import test_utils.TestDataStore;
+package griffinapi;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class IntegrationTest {
+import test_utils.TestDataStore;
+import test_utils.TestInputConfig;
+import test_utils.TestOutputConfig;
+
+public class ComputeEngineIntegrationTest {
 
     @Test
-    public void testComputeEngineIntegration() {
-        List<Integer> inputData = Arrays.asList(1, 10, 25);
+    public void testComputeEngineWithDataStore() {
+        TestInputConfig inputConfig = new TestInputConfig(Arrays.asList(1, 10, 25));
 
-        TestInputConfig inputConfig = new TestInputConfig(inputData);
         TestOutputConfig outputConfig = new TestOutputConfig();
 
         TestDataStore dataStore = new TestDataStore(inputConfig, outputConfig);
 
-        InternalComputeReadingInterface reader = new InternalComputeReading();
+        InternalComputeEngine engine = new InternalComputeEngine();
+        
+        engine.setDataStore(dataStore);
 
-        InternalComputeEngineInterface engine = new InternalComputeEngine(reader);
-
-        for (Integer input : dataStore.readInputData()) {
-            reader.setData(input);
-
-            int result = engine.compute(0); // The parameter is not used in compute()
-
-            dataStore.writeOutputData(String.valueOf(result));
+        List<Integer> inputData = dataStore.readInputData();
+        for (Integer input : inputData) {
+            engine.setData(input);
+            int result = engine.compute(input);
+            String output = "ComputedValue" + result;
+            dataStore.writeOutputData(output);
         }
 
-        // Expected outputs for Fibonacci of [1, 10, 25]
-        List<String> expectedOutput = Arrays.asList("1", "55", "75025");
-
-        List<String> actualOutput = outputConfig.getOutputData();
-
-        assertEquals("Output data does not match expected results", expectedOutput, actualOutput);
+        List<String> expectedOutput = Arrays.asList("ComputedValue1", "ComputedValue1", "ComputedValue1");
+        assertEquals(expectedOutput, outputConfig.getOutputData());
     }
 }
