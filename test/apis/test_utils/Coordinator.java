ff8203060f91c03c2e2b598deb253e6d757bc309
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-27 09:53:33 -0400

    Create Coordinator.java

---
+++ test/apis/test_utils/Coordinator.java
@@ -0,0 +1,21 @@
+package test_utils;
+
+import java.util.List;
+
+public class Coordinator {
+
+    private InternalComputeEngine computeEngine;
+    
+    public Coordinator(InternalComputeEngine computeEngine) {
+        this.computeEngine = computeEngine;
+    }
+
+    public void setData(List<Integer> data) {
+        this.data = data;
+    }
+
+    public void sendDataToComputeEngine() {
+        System.out.println("Coordinator: Sending data to Compute Engine.");
+        computeEngine.receiveData(data);
+    }
+}
