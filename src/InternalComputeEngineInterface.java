295f2b24a696bb701f2ff0978c1459f0f04992f8
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-27 09:49:40 -0400

    Update InternalComputeEngineInterface.java

--- src/InternalComputeEngineInterface.java
+++ src/InternalComputeEngineInterface.java
@@ -1,8 +1,11 @@
+package griffinapi;
+
 interface InternalComputeEngineInterface {
     // Declaring Methods with no bodies
     void setData(int i);
     int getData();
     int compute(int i);
+    int readInData(String i);
+
     
 }
- 
