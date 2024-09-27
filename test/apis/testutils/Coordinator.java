f8c372eac878e6e4d3a02281e7d20c110480995e
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-27 10:44:08 -0400

    Update Coordinator.java

--- test/apis/testutils/Coordinator.java
+++ test/apis/testutils/Coordinator.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Coordinator {
 
     private InternalComputeEngine computeEngine;
+    private List<Integer> data;
     
     public Coordinator(InternalComputeEngine computeEngine) {
         this.computeEngine = computeEngine;
