767a2d03d020fa6a149b7db0cae6e0cb5e33c3e3
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-28 16:02:46 -0400

    Update InternalComputeEngineInterface.java

--- src/apis/ce/InternalComputeEngineInterface.java
+++ src/apis/ce/InternalComputeEngineInterface.java
@@ -4,7 +4,7 @@ public interface InternalComputeEngineInterface {
     // Declaring Methods with no bodies
     void setData(int i);
     int getData();
-    int compute(int i);
+    int computeNthFibonacci(int i);
     int readInData(String i);
 
     
