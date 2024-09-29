d8fd6708bda624f85de75ecbfc01c39dfa13e817
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-28 22:15:01 -0400

    Create IntegrationTest.java

---
+++ src/IntegrationTest.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.Arrays;
+import testutils.Coordinator;
+
+/*
+import testutils.InternalComputeEngine;
+import testutils.DataStore;
+import testutils.InternalComputeEngine
+import testutils.InternalComputeEngine
+*/
+
+public class IntegrationTest {
+
+    public static void main(String[] args) {
+        // Start by Initializing DataStore 
+        ds.DataStore dataStore = new ds.DataStore();
+
+        // Initialize InternalComputeEngine with DataStore
+        apis.ce.InternalComputeEngine computeEngine = new apis.ce.InternalComputeEngine();
+        //HEY THIS LINE HAD A PARAMETER DATASTORE! I REMOVED IT BECAUSE ITS NOT IN THE CONSTRUCTOR!!^^^^
+
+        // Initialize Coordinator with InternalComputeEngine
+        Coordinator coordinator = new Coordinator(computeEngine);
+
+        // Set data in Coordinator
+        List<Integer> dataList = Arrays.asList(1, 10, 25);
+        coordinator.setData(dataList);
+        System.out.println("Coordinator: Data set to " + dataList);
+
+        // Start the data flow
+        coordinator.sendDataToComputeEngine();
+
+        // Retrieve data from DataStore
+        int finalResult = dataStore.getStoredData();
+        System.out.println("Integration Test: Final result in Data Store is " + finalResult);
+    }
+}
