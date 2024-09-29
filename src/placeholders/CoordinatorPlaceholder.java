169086a659d4a3c442cb94ed45cfbc3140457121
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-29 14:23:22 -0400

    Update CoordinatorPlaceholder.java

--- src/placeholders/CoordinatorPlaceholder.java
+++ src/placeholders/CoordinatorPlaceholder.java
@@ -6,12 +6,8 @@ import apis.ce.InternalComputeEngine;
 
 public class CoordinatorPlaceholder {
 
-    private InternalComputeEngine computeEngine;
     private Integer data;
     
-    public CoordinatorPlaceholder(InternalComputeEngine computeEngine) {
-        this.computeEngine = computeEngine;
-    }
 
     public void setData(Integer data) {
         this.data = data;
