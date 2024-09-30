cdf0df35cecdfd5c0cdf5cd9be61dbc0f70201b3
joneserin650 <joneserin650@gmail.com>
2024-09-30 17:01:34 -0400

    refactor: moved the Input and Output configs so other apis can access them with the same structure

--- src/inputoutput/OutputConfig.java
+++ src/inputoutput/OutputConfig.java
@@ -1,23 +1,24 @@
-package apis.ds;
-
-import inputoutput.OutputType;
+package inputoutput;
 
 import java.io.File;
+import java.nio.file.Path;
 
 public class OutputConfig {
 //    this is necessary so the request to write to files doesn't need a million different variables
 //    also makes it so we can expand the requests without breaking old constructors
-    private final File OUTPUT_FILE;
+    private final String OUTPUT_FILE_NAME;
+    private final Path OUTPUT_PATH;
     private final OutputType OUTPUT_TYPE;
 
 
-    public OutputConfig(File OUTPUT_FILE, OutputType OUTPUT_TYPE) {
-        this.OUTPUT_FILE = OUTPUT_FILE;
+    public OutputConfig(String OUTPUT_FILE_NAME, Path OUTPUT_PATH, OutputType OUTPUT_TYPE) {
+        this.OUTPUT_FILE_NAME = OUTPUT_FILE_NAME;
         this.OUTPUT_TYPE = OUTPUT_TYPE;
+        this.OUTPUT_PATH = OUTPUT_PATH;
     }
 
-    public File getOUTPUT_FILE() {
-        return OUTPUT_FILE;
+    public String getOUTPUT_FILE_NAME() {
+        return OUTPUT_FILE_NAME;
     }
 
     public OutputType getOUTPUT_TYPE() {
