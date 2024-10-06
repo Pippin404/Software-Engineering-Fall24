c272529ca5ead9ab70012902ad32db6c70d8be15
joneserin650 <joneserin650@gmail.com>
2024-10-05 20:48:57 -0400

    feat: added the unique codes to the response

--- src/apis/ds/ParseInputFileResponse.java
+++ src/apis/ds/ParseInputFileResponse.java
@@ -1,17 +1,32 @@
 package apis.ds;
 
+import statuscodes.BasicResponseCode;
+import statuscodes.ParseInputFileResponseCode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParseInputFileResponse {
 //    should include the raw integers from the parsed file so the CE can access them
-    List<Integer> parsedIntegers = new ArrayList<>();
+    List<Integer> parsedIntegers;
+    ParseInputFileResponseCode parseInputFileResponseCode;
+    BasicResponseCode basicResponseCode;
 
-    public ParseInputFileResponse(List<Integer> parsedIntegers) {
+    public ParseInputFileResponse(List<Integer> parsedIntegers, ParseInputFileResponseCode parseInputFileResponseCode, BasicResponseCode basicResponseCode) {
         this.parsedIntegers = parsedIntegers;
+        this.parseInputFileResponseCode = parseInputFileResponseCode;
+        this.basicResponseCode = basicResponseCode;
     }
 
     public List<Integer> getParsedIntegers() {
         return parsedIntegers;
+    }
+
+    public ParseInputFileResponseCode getParseInputFileResponseCode() {
+        return parseInputFileResponseCode;
+    }
+
+    public BasicResponseCode getBasicResponseCode() {
+        return basicResponseCode;
     }
 }
