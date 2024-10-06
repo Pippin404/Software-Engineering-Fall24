dc1be0525056b561986ca42a0a99b7f0b5d3ec89
joneserin650 <joneserin650@gmail.com>
2024-10-05 20:47:56 -0400

    feat: checks the parameters before assigning and sends proper exceptions if they're null or an invalid file

--- src/apis/ds/ParseInputFileRequest.java
+++ src/apis/ds/ParseInputFileRequest.java
@@ -2,17 +2,21 @@ package apis.ds;
 
 import inputoutput.Delimiter;
 import inputoutput.InputType;
+import statuscodes.BasicResponseCode;
+import statuscodes.ParseInputFileResponseCode;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.FileNotFoundException;
 
 public class ParseInputFileRequest  {
 //    should include an input config with the file path and the delimiters
     private File inputFile;
     private Delimiter delimiter;
     private InputType inputType;
+
+//    status codes for error handling
+    private BasicResponseCode basicResponseCode;
+    private ParseInputFileResponseCode parseInputFileResponseCode;
 
     public File getInputFile() {
         return inputFile;
@@ -26,7 +30,51 @@ public class ParseInputFileRequest  {
         return inputType;
     }
 
+    public BasicResponseCode getBasicResponseCode() {
+        return basicResponseCode;
+    }
+
+    public ParseInputFileResponseCode getParseInputFileResponseCode() {
+        return parseInputFileResponseCode;
+    }
+
     public ParseInputFileRequest(File inputFile, InputType inputType, Delimiter delimiter) {
+        try {
+//            inputFile cannot point to an invalid directory
+//            if we check for inputFile.exists() first it would have a null pointer problem
+            if(inputFile == null) {
+                basicResponseCode = BasicResponseCode.NULL_PARAMETER;
+                throw new IllegalArgumentException("Input file cannot be null.");
+            }
+            else if(inputFile.exists()) {
+                parseInputFileResponseCode = ParseInputFileResponseCode.VALID_FILE;
+                this.inputFile = inputFile;
+            } else {
+                parseInputFileResponseCode = ParseInputFileResponseCode.INVALID_FILE;
+                throw new FileNotFoundException("Input file not found. File Path: " + inputFile);
+            }
+
+//            inputType cannot be null
+            if(inputType != null) {
+                this.inputType = inputType;
+            } else {
+                throw new IllegalArgumentException("Input type cannot be null.");
+            }
+//            delimiter cannot be null
+            if(delimiter != null) {
+                this.delimiter = delimiter;
+            } else {
+                basicResponseCode = BasicResponseCode.NULL_PARAMETER;
+                throw new IllegalArgumentException("Delimiter type cannot be null.");
+            }
+            basicResponseCode = BasicResponseCode.VALID_PARAMETERS;
+
+        } catch(FileNotFoundException e) {
+            System.out.println("ParseInputFileCodes: " + this.parseInputFileResponseCode);
+            e.printStackTrace();
+        } catch(IllegalArgumentException e) {
+            e.printStackTrace();
+        }
         this.inputFile = inputFile;
         this.inputType = inputType;
         this.delimiter = delimiter;
