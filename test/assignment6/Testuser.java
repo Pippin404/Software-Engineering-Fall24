088d5f3424179c6d56446eaf40fd3b1288d58ad2
joneserin650 <joneserin650@gmail.com>
2024-10-26 18:45:48 -0400

    misc: changed todo names to be clearer

--- test/assignment6/Testuser.java
+++ test/assignment6/Testuser.java
@@ -8,7 +8,7 @@ import java.io.File;
 
 public class Testuser {
 	
-	// DONE TODO 3: change the type of this variable to the name you're using for your
+	// DONE 6.C.3: change the type of this variable to the name you're using for your
 	// User <-> ComputeEngine API; also update the parameter passed to the constructor DONE
     //Pippin Notes: There isn't a toString function or anything. I wonder how our code is going to integrate with this
 	private final UScomputerEngineConstructer coordinator;
@@ -21,7 +21,7 @@ public class Testuser {
 		char delimiter = ';';
 		String inputPath = "test" + File.separatorChar + "testInputFile.test";
 
-		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+		// TODO 6.C.4: Call the appropriate method(s) on the coordinator to get it to
 		// run the compute job specified by inputPath, outputPath, and delimiter
 	}
 
