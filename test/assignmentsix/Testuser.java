06f835a767eecb1bd013dd5086f9da9f474ac169
Erin Jones <158182442+jones-erin-650@users.noreply.github.com>
2024-10-28 00:21:59 -0400

    feat: skeleton of the User test for when the coordinator is done (#123)

--- test/assignmentsix/Testuser.java
+++ test/assignmentsix/Testuser.java
@@ -1,6 +1,8 @@
 package assignmentsix;
 
 
+import apis.ce.InternalComputeEngine;
+import apis.ds.DataStore;
 import apis.us.UScomputerEngineConstructer;
 
 import java.io.File;
@@ -18,11 +20,34 @@ public class Testuser {
 	}
 
 	public void run(String outputPath) {
-		char delimiter = ';';
+		char delimiter = '.';
 		String inputPath = "test" + File.separatorChar + "testInputFile.test";
 
 		// TODO 6.C.4: Call the appropriate method(s) on the coordinator to get it to
 		// run the compute job specified by inputPath, outputPath, and delimiter
+		// Initializing template datastore
+		apis.ds.DataStore dataStore = new DataStore();
+
+		// Initializing internal compute engine with template datastore
+
+		InternalComputeEngine computeEngine = new InternalComputeEngine();
+
+		// Initialize Coordinator with InternalComputeEngine
+		UScomputerEngineConstructer coordinator = new UScomputerEngineConstructer(computeEngine, dataStore);
+
+		Testuser testuser = new Testuser(coordinator);
+
+//		TODO: Call the Coordinator's methods for parsing the file and computing the data
+
+		File inputFile = new File(inputPath);
+
+		coordinator.setInputFile(inputFile);
+
+//		List<Integer> parsedIntegers = coordinator.parseInputFile();
+
+//		Foo results = Call the compute engine to do the calculation
+
+//		coordinator.writeToFile(outputFile, results)
 	}
 
 }
