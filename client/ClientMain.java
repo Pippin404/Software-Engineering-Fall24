7d3a6e480fd28900975abc96ae21042b9b3ee5b6
Pippin404 <Baconpippin@gmail.com>
2024-11-03 12:40:51 -0500

    Added beginner protobuf. changed location of SRCIntegrationtest

---
+++ client/ClientMain.java
@@ -0,0 +1,20 @@
+package prototest;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hey this is the client!");
+        
+        //give me a filename thru scanner
+        
+        //give that filename to the protobuff
+        
+        //ask about location 
+        
+        //send protobuff!
+        
+        
+    }
+    
+}
