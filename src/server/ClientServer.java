0e3f1fa3d4b33742e8002aaae136e2557bb3e0df
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-11-04 15:23:56 -0500

    Created ClientServer.java
    ClientServer.java gets request from client and activates the senddatatoclient method in proto. It initializes all the parts of the API, gets them to do the computation, then it returns a message back to the client telling them whether it was successful and what the output type is. (I havent actually put in any error messages yet)

---
+++ src/server/ClientServer.java
@@ -0,0 +1,68 @@
+package server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import protobuf.Clientserver.sendclientserver;
+import protobuf.Clientserver.sendresponse;
+import protobuf.SenddataGrpc;
+import apis.ce.InternalComputeEngine;
+import apis.ds.DataStore;
+import apis.us.UScomputerEngineConstructer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class ClientServer extends SenddataGrpc.SenddataImplBase {
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+      // Start the gRPC server
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new ClientServer())
+                .build()
+                .start();
+
+        System.out.println("Server started on port 50051");
+
+        // Keep the server running
+        server.awaitTermination();
+  }
+
+
+  @Override
+  // follows the pattern outlined in the senddatatoclient method that was generated from proto (SenddataGrpc.java)
+  public void senddatatoclient(sendclientserver request, StreamObserver<sendresponse> responseObserver) {
+  
+    String fileLocation = request.getFileLocation();
+    sendclientserver.outLocation outputLocation = request.getOutputLocation();
+
+    // Initialize necessary components (DataStore, InternalComputeEngine, etc.)
+    DataStore dataStore = new DataStore();
+    InternalComputeEngine computeEngine = new InternalComputeEngine();
+    UScomputerEngineConstructer coordinator = new UScomputerEngineConstructer(computeEngine, dataStore);
+
+    // Set up the input file and process it
+    File inputFile = new File(fileLocation);
+    coordinator.setInputFile(inputFile);
+    coordinator.setData();
+
+    // Perform computation
+    List<Integer> result = coordinator.runInternalCompute(coordinator.getData());
+    dataStore.setData(result);
+
+    // Create and send a response
+    sendresponse response = sendresponse.newBuilder()
+                        // Need to add other responses for errors
+            .setMessage("File accepted. Output Type: "+ outputLocation +"."); 
+            .build();
+
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+
+    
+  }
+
+  
+
+}
