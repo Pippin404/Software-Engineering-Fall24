f2433f7eb24f8d75ba954497b852b66bb9e0b5b8
eribrin <joneserin650@gmail.com>
2024-12-16 19:38:51 -0500

    checkstyles >:(

--- test/ds/unittests/ParseFileBuilding.java
+++ test/ds/unittests/ParseFileBuilding.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class ParseFileBuilding {
 
